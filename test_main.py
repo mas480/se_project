280132bf553c6f9bc1e91ff2f9b57da22a43b33b
mas480 <122989623+mas480@users.noreply.github.com>
2023-06-19 12:58:22 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,7 @@
 import requests
 from main import load_model
 
-
-def load_pipeline():
-    model_gpt2 = load_model()
-    return model_gpt2
+model_gpt2 = load_model()
 
 
 def test_connect_streamlit():
@@ -12,15 +9,15 @@ def test_connect_streamlit():
     assert x.status_code == 200
 
 
-def test_type_model(load_pipeline()):
-    assert str(type(load_model)) == "<class 'transformers.pipelines.text_generation.TextGenerationPipeline'>"
+def test_type_model(model_gpt2):
+    assert str(type(model_gpt2)) == "<class 'transformers.pipelines.text_generation.TextGenerationPipeline'>"
 
 
-def test_example_1(load_pipeline()):
-    x = load_model('Сегодня')[0]['generated_text']
+def test_example_1(model_gpt2):
+    x = model_gpt2('Сегодня')[0]['generated_text']
     assert x == "Сегодня, в день рождения, я хочу поздравить всех, кто любит и ценит свою Родину, кто"
 
 
-def test_example_2(load_pipeline()):
-    x = load_model('Завтра')[0]['generated_text']
+def test_example_2(model_gpt2):
+    x = model_gpt2('Завтра')[0]['generated_text']
     assert x == "Утром, когда я проснулся, я увидел, что в комнате горит свет. Я подумал, что это"
