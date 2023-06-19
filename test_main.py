2b2cae99800d2e6d7f15b8586468bbc48b7bc23a
mas480 <122989623+mas480@users.noreply.github.com>
2023-06-19 13:52:34 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import requests
-from transformers import pipeline
+from main import load_model
 
 
 def test_connect_streamlit():
@@ -13,22 +13,22 @@ def test_connect_model():
     
     
 def load_model_check():
-    model = pipeline(model="ai-forever/rugpt3large_based_on_gpt2")
+    model = load_model()
     return model
 
 
-model_gpt2 = load_model_check()
-
-
 def test_type_model(model_gpt2):
+    model_gpt2 = load_model_check()
     assert str(type(model_gpt2)) == "<class 'transformers.pipelines.text_generation.TextGenerationPipeline'>"
 
 
 def test_example_1(model_gpt2):
+    model_gpt2 = load_model_check()
     x = model_gpt2('Сегодня')[0]['generated_text']
     assert x == "Сегодня, в день рождения, я хочу поздравить всех, кто любит и ценит свою Родину, кто"
 
 
 def test_example_2(model_gpt2):
+    model_gpt2 = load_model_check()
     x = model_gpt2('Завтра')[0]['generated_text']
     assert x == "Утром, когда я проснулся, я увидел, что в комнате горит свет. Я подумал, что это"
